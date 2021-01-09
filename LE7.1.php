735c1518b281d5abe416c7f51d798b5014587ef2
jakesipe123 <60919213+jakesipe123@users.noreply.github.com>
2021-01-09 16:49:24 +0800

    Update LE7.1.php

--- LE7.1.php
+++ LE7.1.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PORTFOLIO</title>
-    <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href="./css/sidebar.css">
-    <link rel="stylesheet" href="./css/canvas.css">
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./sidebar.css">
+    <link rel="stylesheet" href="./canvas.css">
 </head>
 <body>
     <div class="navbar">
