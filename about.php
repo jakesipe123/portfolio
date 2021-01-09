a46698e3102f40fd6c59141e95c6536522558dd0
jakesipe123 <60919213+jakesipe123@users.noreply.github.com>
2021-01-09 16:45:56 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORTFOLIO</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/about.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="css.php">CSS</a>
+        <a href="svg.php">SVG</a>
+        <a href="canvas.php">Canvas</a>
+        <a href="about.php">About</a>
+    </div>
+    <div id="content" class="container">
+    The best way to get started is to quit talking and begin doing. I am Francis Jake Sipe, a student of BSIT 3C and this is the culmination of all contents I've made in web animation. It doesn't look good like others but I'm still working to do it well and make it better.
+    </div>
+</body>
+</html>
